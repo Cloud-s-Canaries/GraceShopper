cd36e22d625b3b909bee73ba91661423ee04abe8
anclaudys <anclaudysdata@gmail.com>
2021-03-03 16:22:15 -0500

    Added features to cart

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -1,27 +1,64 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {getCartItemsThunk, addToCartThunk} from '../store/cart'
+import {getCartItemsThunk, updateQuantityThunk} from '../store/cart'
 
 class Cart extends React.Component {
   constructor() {
     super()
+    this.state = {
+      itemQuant: 1
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleChange(evt) {
+    this.setState({itemQuant: evt.target.value})
+  }
+
   componentDidMount() {
     this.props.loadCartItems(this.props.match.params.userID)
   }
 
+  handleSubmit(itemID) {
+    this.props.updateQuant(
+      this.props.match.params.userID,
+      itemID,
+      this.state.itemQuant
+    )
+  }
+
   render() {
     const cartItems = this.props.cartItems || []
+    const optionsArr = Array(25).fill(1)
+
     return (
       <div>
         {' '}
         {cartItems.length ? (
           cartItems.map(item => {
+            console.log(`ITEM`, item)
             return (
               <div key={item.id}>
-                <> {item.name} </>
-                <> {item.price}</>
-                <> Quantity: {item.cart.quantity} </>
+                <div> {item.name} </div>
+                <div> {item.price}</div>
+                <img src={`../images/${item.imageUrl}`} />
+                <div> Quantity: {item.cart ? item.cart.quantity : 1} </div>
+                <label htmlFor="quantity">Select Quantity</label>
+                <select
+                  name="quantity"
+                  value={this.state.itemQuant}
+                  onChange={this.handleChange}
+                >
+                  {optionsArr.map((val, idx) => {
+                    return <option value={val + idx}> {val + idx} </option>
+                  })}
+                </select>
+                <button onClick={() => this.handleSubmit(item.id)}>
+                  {' '}
+                  Change
+                </button>
+                <br />
               </div>
             )
           })
@@ -41,7 +78,9 @@ const mapState = state => {
 
 const mapDispatch = dispatch => {
   return {
-    loadCartItems: userID => dispatch(getCartItemsThunk(userID))
+    loadCartItems: userID => dispatch(getCartItemsThunk(userID)),
+    updateQuant: (userID, itemID, quant) =>
+      dispatch(updateQuantityThunk(userID, itemID, quant))
   }
 }
 
